6dc01f5f9a90f45e0f87b02d48931261d8da66b6
Mickey Donaghy <michael.donaghy@linecorp.com>
2020-10-02 14:51:53 +0900

    Add test

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,11 @@ plugins {
 repositories {
     mavenCentral()
 }
+dependencies {
+    api(kotlin("stdlib-jdk8"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+}
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
